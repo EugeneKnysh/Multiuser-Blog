d3d42ed60456cfa8c8b52fc8d3321f2081913e68
Eugene Knysh <eugeneknysh21@gmail.com>
2021-03-30 10:21:22 +0300

    Add CookieUtils class.

---
+++ src/main/java/com/gmail/eugeneknysh21/blog/utility/CookieUtils.java
@@ -0,0 +1,35 @@
+package com.gmail.eugeneknysh21.blog.utility;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
+public class CookieUtils {
+    public static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
+        Cookie[] cookies = request.getCookies();
+        Optional<Cookie> result = Optional.empty();
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(name)) {
+                    result = Optional.of(cookie);
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void removeCookie(HttpServletRequest request, HttpServletResponse response, String name) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(name)) {
+                    cookie.setValue("");
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                    break;
+                }
+            }
+        }
+    }
+}
